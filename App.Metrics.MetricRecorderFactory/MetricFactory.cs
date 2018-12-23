fb6a88d8dc97d09de68ccd088573ab265a563f93
Oleksandr <avkomq@gmail.com>
2018-12-23 16:50:17 -0500

    Add solution

---
+++ App.Metrics.MetricRecorderFactory/MetricFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App.Metrics.MetricRecorderFactory
+{
+    public class MetricFactory
+    {
+        public MetricFactory()
+        {
+            
+        }
+
+        public TMetric CreateMetric<TMetric>()
+        {
+            return default(TMetric);
+        }
+    }
+}

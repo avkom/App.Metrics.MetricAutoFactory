b42a4c7469f2b651d8a459fafaedf65232df644d
Oleksandr <avkomq@gmail.com>
2018-12-23 22:33:47 -0500

    Add support for Gauge metric type.

--- App.Metrics.MetricAutoFactory.Tests/MetricFactoryTests.cs
+++ App.Metrics.MetricAutoFactory.Tests/MetricFactoryTests.cs
@@ -1,23 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using App.Metrics.Counter;
+using App.Metrics.Gauge;
 
 namespace App.Metrics.MetricAutoFactory.Tests
 {
     [TestClass]
     public class MetricFactoryTests
     {
-        private Mock<ICounter> _counterMock;
-        private Mock<IProvideCounterMetrics> _provideCounterMetricsMock;
         private Mock<IProvideMetrics> _metricsProviderMock;
         private Mock<IMetrics> _metricsMock;
-        private IMetricFactory _metricFactory;
 
         [TestInitialize]
         public void InitializeTest()
         {
-            _counterMock = new Mock<ICounter>();
-            _provideCounterMetricsMock = new Mock<IProvideCounterMetrics>();
             _metricsProviderMock = new Mock<IProvideMetrics>();
             _metricsMock = new Mock<IMetrics>();
         }
@@ -26,39 +22,41 @@ namespace App.Metrics.MetricAutoFactory.Tests
         public void TestCounterIncrement()
         {
             // Arrange
-            _counterMock
+            var counterMock = new Mock<ICounter>();
+            counterMock
                 .Setup(m => m.Increment(It.IsAny<long>()));
 
-            _provideCounterMetricsMock
+            var provideCounterMetricsMock = new Mock<IProvideCounterMetrics>();
+            provideCounterMetricsMock
                 .Setup(m => m.Instance(It.IsAny<CounterOptions>(), It.IsAny<MetricTags>()))
-                .Returns(_counterMock.Object);
+                .Returns(counterMock.Object);
 
             _metricsProviderMock
                 .SetupGet(m => m.Counter)
-                .Returns(_provideCounterMetricsMock.Object);
+                .Returns(provideCounterMetricsMock.Object);
 
             _metricsMock
                 .SetupGet(m => m.Provider)
                 .Returns(_metricsProviderMock.Object);
 
-            _metricFactory = new MetricFactory(_metricsMock.Object);
+            var metricFactory = new MetricFactory(_metricsMock.Object);
 
-            ITestMetric1 testMetric1 = _metricFactory.CreateMetric<ITestMetric1>();
+            ITestMetric1 testMetric1 = metricFactory.CreateMetric<ITestMetric1>();
 
             // Act
             testMetric1.RequestCount("endpoint1", 200).Increment(5);
 
             // Assert
-            _counterMock.VerifyAll();
-            _provideCounterMetricsMock.VerifyAll();
+            counterMock.VerifyAll();
+            provideCounterMetricsMock.VerifyAll();
             _metricsProviderMock.VerifyAll();
             _metricsMock.VerifyAll();
 
-            Assert.AreEqual(1, _provideCounterMetricsMock.Invocations.Count);
-            CounterOptions counterOptions = (CounterOptions) _provideCounterMetricsMock.Invocations[0].Arguments[0];
+            Assert.AreEqual(1, provideCounterMetricsMock.Invocations.Count);
+            CounterOptions counterOptions = (CounterOptions) provideCounterMetricsMock.Invocations[0].Arguments[0];
             Assert.AreEqual("RequestCount", counterOptions.Name);
 
-            MetricTags metricTags = (MetricTags) _provideCounterMetricsMock.Invocations[0].Arguments[1];
+            MetricTags metricTags = (MetricTags) provideCounterMetricsMock.Invocations[0].Arguments[1];
             Assert.AreEqual(2, metricTags.Keys.Length);
             Assert.AreEqual("endpoint", metricTags.Keys[0]);
             Assert.AreEqual("httpStatus", metricTags.Keys[1]);
@@ -67,12 +65,61 @@ namespace App.Metrics.MetricAutoFactory.Tests
             Assert.AreEqual("endpoint1", metricTags.Values[0]);
             Assert.AreEqual("200", metricTags.Values[1]);
 
-            Assert.AreEqual(5L, _counterMock.Invocations[0].Arguments[0]);
+            Assert.AreEqual(5L, counterMock.Invocations[0].Arguments[0]);
+        }
+
+        [TestMethod]
+        public void TestGaugeSetValue()
+        {
+            // Arrange
+            var gaugeMock = new Mock<IGauge>();
+            gaugeMock
+                .Setup(m => m.SetValue(It.IsAny<double>()));
+
+            var provideGaugeMetricsMock = new Mock<IProvideGaugeMetrics>();
+            provideGaugeMetricsMock
+                .Setup(m => m.Instance(It.IsAny<GaugeOptions>(), It.IsAny<MetricTags>()))
+                .Returns(gaugeMock.Object);
+
+            _metricsProviderMock
+                .SetupGet(m => m.Gauge)
+                .Returns(provideGaugeMetricsMock.Object);
+
+            _metricsMock
+                .SetupGet(m => m.Provider)
+                .Returns(_metricsProviderMock.Object);
+
+            var metricFactory = new MetricFactory(_metricsMock.Object);
+
+            ITestMetric1 testMetric1 = metricFactory.CreateMetric<ITestMetric1>();
+
+            // Act
+            testMetric1.QueueLength("queue1").SetValue(5.0);
+
+            // Assert
+            gaugeMock.VerifyAll();
+            provideGaugeMetricsMock.VerifyAll();
+            _metricsProviderMock.VerifyAll();
+            _metricsMock.VerifyAll();
+
+            Assert.AreEqual(1, provideGaugeMetricsMock.Invocations.Count);
+            GaugeOptions gaugeOptions = (GaugeOptions) provideGaugeMetricsMock.Invocations[0].Arguments[0];
+            Assert.AreEqual("QueueLength", gaugeOptions.Name);
+
+            MetricTags metricTags = (MetricTags)provideGaugeMetricsMock.Invocations[0].Arguments[1];
+            Assert.AreEqual(1, metricTags.Keys.Length);
+            Assert.AreEqual("queue", metricTags.Keys[0]);
+
+            Assert.AreEqual(1, metricTags.Values.Length);
+            Assert.AreEqual("queue1", metricTags.Values[0]);
+
+            Assert.AreEqual(5.0, gaugeMock.Invocations[0].Arguments[0]);
         }
 
         public interface ITestMetric1
         {
             ICounter RequestCount(string endpoint, int httpStatus);
+            IGauge QueueLength(string queue);
         }
     }
 }
--- App.Metrics.MetricAutoFactory/MetricInterceptor.cs
+++ App.Metrics.MetricAutoFactory/MetricInterceptor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using App.Metrics.Counter;
+using App.Metrics.Gauge;
 using Castle.DynamicProxy;
 // ReSharper disable SuspiciousTypeConversion.Global
 
@@ -22,11 +23,25 @@ namespace App.Metrics.MetricAutoFactory
                 {
                     Name = invocation.Method.Name
                 };
-                string[] tagKeys = invocation.Method.GetParameters().Select(parameter => parameter.Name).ToArray();
-                string[] tagValues = invocation.Arguments.Select(argument => argument.ToString()).ToArray();
-                MetricTags metricTags = new MetricTags(tagKeys, tagValues);
+                MetricTags metricTags = GetTags(invocation);
                 invocation.ReturnValue = _metricsProvider.Counter.Instance(counterOptions, metricTags);
             }
+            else if (invocation.Method.ReturnType == typeof(IGauge))
+            {
+                GaugeOptions counterOptions = new GaugeOptions
+                {
+                    Name = invocation.Method.Name
+                };
+                MetricTags metricTags = GetTags(invocation);
+                invocation.ReturnValue = _metricsProvider.Gauge.Instance(counterOptions, metricTags);
+            }
+        }
+
+        private MetricTags GetTags(IInvocation invocation)
+        {
+            string[] tagKeys = invocation.Method.GetParameters().Select(parameter => parameter.Name).ToArray();
+            string[] tagValues = invocation.Arguments.Select(argument => argument.ToString()).ToArray();
+            return new MetricTags(tagKeys, tagValues);
         }
     }
 }
